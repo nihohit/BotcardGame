93be6bd6cd26cfa3e701751d896d9bab604aff80
Shachar Langbeheim <nihohit@gmail.com>
2021-07-19 21:06:03 +0300

    Add empty scripts to handle UI inputs.

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MainController : MonoBehaviour {
+    private List<SystemPanelScript> _systemPanels;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _systemPanels = FindObjectsOfType<SystemPanelScript>().OrderBy((systemPanel) => systemPanel.gameObject.name).ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
