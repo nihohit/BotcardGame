d695a36d6db02a9f17a132f518137bcd86154fd9
Shachar Langbeheim <nihohit@gmail.com>
2021-08-02 21:28:20 +0300

    Added board creation.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -17,8 +17,20 @@ public class BoardContent {
 public class Board {
   private BoardContent[,] content;
 
+  private int xSize {
+    get {
+      return content.GetLength(0);
+    }
+  }
+
+  private int ySize {
+    get {
+      return content.GetLength(1);
+    }
+  }
+
   public Vector2Int getSize() {
-    return new Vector2Int(content.GetLength(0), content.GetLength(1));
+    return new Vector2Int(xSize, ySize);
   }
 
   public BoardContent ContentAt(Vector2Int position) {
@@ -30,20 +42,40 @@ public class Board {
   }
 
   private BoardContent[,] copyBoard() {
-    var size = getSize();
-    var newContent = new BoardContent[size.x, size.y];
-    for (int i = 0; i < size.x; ++i) {
-      for (int j = 0; j < size.y; ++j) {
+    var newContent = new BoardContent[xSize, ySize];
+    for (int i = 0; i < xSize; ++i) {
+      for (int j = 0; j < ySize; ++j) {
         newContent[i, j] = content[i, j].Copy();
       }
     }
     return newContent;
   }
 
+  public static Board CreateBoard(Vector2Int size, IEnumerable<Tuple<BoardContent, Vector2Int>> insertedContent) {
+    var nextBoard = new Board();
+    nextBoard.content = new BoardContent[size.x, size.y];
+
+    foreach (var pair in insertedContent) {
+      var position = pair.Item2;
+      var content = pair.Item1;
+      Debug.Assert(nextBoard.withinBounds(position));
+      Debug.Assert(nextBoard.ContentAt(position) is null);
+      nextBoard.putContentAt(position, content);
+    }
+
+    return nextBoard;
+  }
+
+  private bool withinBounds(Vector2Int position) {
+    return position.x >= 0 &&
+          position.y >= 0 &&
+          position.x < xSize &&
+          position.y < ySize;
+  }
+
   public Board NextBoard(IEnumerable<ActionEffect> effects) {
     var nextBoard = new Board();
     nextBoard.content = copyBoard();
-    var size = getSize();
 
     foreach (var effect in effects) {
       var contentInTile = ContentAt(effect.position);
@@ -57,11 +89,7 @@ public class Board {
       for (int i = 1; i < maxValue; ++i) {
         var adjustedMove = effect.move * i / maxValue;
         var position = effect.position + adjustedMove;
-        if (position.x >= 0 &&
-            position.y >= 0 &&
-            position.x < size.x &&
-            position.y < size.y &&
-            ContentAt(position) is null) {
+        if (withinBounds(position) && ContentAt(position) is null) {
           finalLocation = position;
         }
       }
